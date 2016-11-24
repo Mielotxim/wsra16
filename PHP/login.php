726a1810d13408568b0d194d165d5add6829d33a
Mielotxim <ekaneiza8@gmail.com>
2016-11-24 10:40:04 +0100

    moar aldaketak

--- PHP/login.php
+++ PHP/login.php
@@ -4,16 +4,23 @@
 		<link rel="stylesheet" href="../css/bootstrap.min.css" />
 		<link rel="stylesheet" href="../css/custom.min.css" />
 	</head>
-	<body>
+	<body style="background-color:#d3d3d3;">
+		
 		<br>
-			<div class="col-md-3  col-md-offset-2">
+			<div class="col-md-offset-2">
+			<form id="formularioa" name="formularioa" method="POST" onSubmit="./login.php">
 				<p>Erabiltzailea: <input type="text" id="erab" placeholder="Erabiltzailea" ></input></p>
 				<p>Pasahitza: <input type="password" id="pass"></input></p>
+				<input type="submit" value="Ok" id="ok"></input>
+			</form>
 			</div>
 		</br>
-	</body>
+		<a href="../home.html">-=HOME=-</a>
+		</body>
 </html>
 
 <?php
-
+	if(isset($_POST['erab'],$_POST['pass']){
+		echo "va bien";
+	}
 ?>
