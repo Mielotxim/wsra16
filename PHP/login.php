8e4086636d4b75dc3a288b1eba7479fa745f7e38
Mielotxim <ekaneiza8@gmail.com>
2016-11-24 10:25:51 +0100

    lehen aldaketak, proiektua badoa ^^

---
+++ PHP/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../css/custom.min.css" />
+	</head>
+	<body>
+		<br>
+			<div class="col-md-3  col-md-offset-2">
+				<p>Erabiltzailea: <input type="text" id="erab" placeholder="Erabiltzailea" ></input></p>
+				<p>Pasahitza: <input type="password" id="pass"></input></p>
+			</div>
+		</br>
+	</body>
+</html>
+
+<?php
+
+?>
