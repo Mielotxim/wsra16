fd4756bd5d806b37c4aeca5616632e01d5594988
Mielotxim <ekaneiza8@gmail.com>
2016-11-28 13:59:30 +0100

    aldaketak photon

--- PHP/photo.php
+++ PHP/photo.php
@@ -4,8 +4,14 @@
 		<link rel="stylesheet" href="../css/bootstrap.min.css" />
 		<link rel="stylesheet" href="../css/custom.min.css" />
 	</head>
-	<body style="background-color:#d3d3d3;">
-		<center><div id="imgL"><img id="irudia1" src="../img/2218017.jpg" style="border-style:solid;boder-color:#48F87C;width:300px;height:300;"></div></center>
+	<body>
+		<center>
+			<div id="imgL"><img id="irudia1" src="../img/2218017.jpg" style="width:300px;height:300;margin-top:20px;">
+				<div style="margin-top:20px;">
+					<input type="button" name="like" id="like" style="border-style:solid;border-color:#123456;" value="like"></input>
+				</div>
+			</div>
+		</center>
 	</body>
 </html>
 
