4dcf6d717fc8dc230ad5cc91e5dfa30e86f24373
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2016-11-28 14:01:23 +0100

    sign up php sortu

---
+++ PHP/erregistro.php
@@ -0,0 +1,25 @@
+<?php
+	$izena=$_POST['izena'];
+	$abizenak="";
+	if(isset($_POST['abizenak']){
+		$abizenak=$_POST['abizenak'];
+	}
+	$eposta=$_POST['eposta'];
+	$pasahitza=$_POST['pass'];
+	$konfirmazioa=$_POST['passKonf'];
+	$patron=array("options"=>array("regexp"=>"/^(\w+)@(\D+)\.(\D+)$/"));
+	
+	if((strlen($izena) > 0) && (strlen($pasahitza) > 5) && (strcmp($pasahitza, $konfirmazioa) == 0) && filter_var($eposta,FILTER_VALIDATE_REGEXP,$patron)) {
+		include "./konektatu.php";
+		
+		$pasahitza = sha1($pasahitza);
+		$sql = "INSERT INTO erabiltzailea(Eposta, Izena, Abizenak, Pasahitza) VALUES ('$eposta','$izena','$abizenak','$pasahitza')";
+		if (!$niremysqli->query($sql)){
+			echo "Taularen sorrerak huts egin: (" .
+			$mysqli->errno . ") " . $mysqli->error;
+			die('Errorea: ' . $niremysqli->error);
+		}
+		else { echo "Erregistro bat gehitu da!";}
+		echo "<p><a href='../home.html'>Home</a></p>";
+	}
+?>
