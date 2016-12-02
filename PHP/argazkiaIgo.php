da9fe62596aaac29658ca2b19ab4a3c95fc3299e
Mielotxim <ekaneiza8@gmail.com>
2016-12-02 12:03:44 +0100

    argazkiak datu basera igo daitezke.

--- PHP/argazkiaIgo.php
+++ PHP/argazkiaIgo.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION['user'])){
+	header('location:../home.html');
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,15 +19,6 @@ session_start();
 				output.src = URL.createObjectURL(event.target.files[0]);
 				output.style.display="inline";
 				};
-			</script>
-			<script>
-				if((document.getElementById("irudi").value=="" && (document.getElementById("private").value!="private" ||document.getElementById("private").value!="public" || document.getElementById("private").value!="friend") ){
-					alert ("algo va mal.");
-					return false;
-				}
-				else{
-					return true;
-				}
 			</script>
 	</head>
 	<body style="background-color:#E6E6E6;">
@@ -46,24 +40,40 @@ session_start();
 				<center><h5>YOUR RUSKY PHOTOS</h5></center>
 			</div>
 		</div>
-		<form  id="erregistro" name="erregistro" method="post" onSubmit="return todoBien()" action="./argazkiaIgo.php" enctype="multipart/form-data">
+		<form  id="erregistro" name="erregistro" method="post" onSubmit="todoBien()"  action="./argazkiaIgo.php" enctype="multipart/form-data">
 			<p>IRUDIA aukeratu: </p><input type="file" accept="image/*" id="irudi" name="irudi" onchange="loadFile(event)"></input>
 			<img id="image" name="image" style="height:400px;width:400px;display:none;position:absolute;right:35%;top:40%;"></img>
-			<br><p>IZENBURUA: </p><input type="text" id="izenburu" name="izenburua" placeholder="TXUPIPARTY EN LA PLAYA"></input></br>
-			<br><p>IZENBURUA: </p><input type="radio" id="private" name="private" value="private">Pribatua</input>
+			<br><p>IZENBURUA: </p><input type="text" id="izenburua" name="izenburua" placeholder="TXUPIPARTY EN LA PLAYA"></input></br>
+			<br><p>PRIBATUTASUNA: </p><input type="radio" id="private" name="private" value="private">Pribatua</input>
 															 <input type="radio" id="private" name="private" value="public">Publikoa</input>
 															 <input type="radio" id="private" name="private" value="friend">Lagunentzako</input></br>
 			<button name="balioztatu" id="balioztatu" style="border-style:solid;border-color:#895895;" value="balioztatu">
-						<span class="glyphicon glyphicon-power" aria-hidden="true"></span>  BAIEZTATU
+						<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>  BAIEZTATU
 					</button>
 		</form>
 	</body>
 </html>
 
 <?php
-	if(isset($_POST['private'],$_FILE['irudi'])){
-	echo "TO JAKEROO !";
-	$argazkia =addslashes(file_get_contents($_FILES['irudi']['tmp_name']));
+	if(isset($_POST['private']) && strlen($_FILES['irudi']['tmp_name'])!=0){
+		echo "TO JAKEROO !";
+		include('konektatu.php');
+		$argazkia =addslashes(file_get_contents($_FILES['irudi']['tmp_name']));
+		$sql = "INSERT INTO argazkia(Argazkia,Kategoria,Titulua,Eposta)
+		VALUES('$argazkia' , '$_POST[private]' , '$_POST[izenburua]' , '$_SESSION[user]')";
+		
+		if (!$niremysqli->query($sql)){
+			echo "Taularen sorrerak huts egin: (" .
+			$mysqli->errno . ") " . $mysqli->error;
+			die('Errorea: ' . $niremysqli->error);
+		}
+		else {
+			echo "OK";
+		}
 	}
+	else{
+		echo "Datuak dagokien informazioekin bete itzazu mesedez";
+	}
+	
 ?>
 		
